eef5300fa96704da96b67db66a5d0156a5957884
Sebastian Sandoval <sebassandoval@hotmail.com>
2023-12-06 18:45:18 -0500

    Update ENV FILE

--- src/APIClient/apiClient.tsx
+++ src/APIClient/apiClient.tsx
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+const BACK_URL = process.env.NEXT_PUBLIC_BACK_URL
+
 export const ApiClient = axios.create({
-  baseURL: process.env.BACK_URL || 'http://localhost:3000/api',
+  baseURL: BACK_URL || 'http://localhost:3000/api',
   headers: {
     'Content-Type': 'application/json',
   },
